b72387d1291fe5281d603002acaea6716430a1a3
YeongJoon <yjk7557@naver.com>
2023-12-28 03:07:09 +0900

    [Feat] 1차 UI 개발

---
+++ MYPLACE-iOS/MYPLACE-iOS/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MYPLACE-iOS
+//
+//  Created by 김영준 on 12/27/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
